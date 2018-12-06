e857ebd5bce1cb5ead55bfed20aca1691c11bea9
Laura Stromboni <laura.stromboni@live.fr>
2018-12-06 17:23:43 +0100

    saved done

--- src/components/Saved.js
+++ src/components/Saved.js
@@ -41,28 +41,38 @@ class Saved extends Component {
       <section className="Saved">
         <h2>Saved</h2>
         <p>Here you can find the places you've saved as favorite.</p>
-        <ul className="col-lg-12">
-        {this.state.favorites.map(oneHouse=>{
-            return(
-                <li key = {oneHouse.houses._id} className="col-lg-3 col-md-4 col-sm-6 col-xs-12">
-                    <Link to={houseUrl(oneHouse.houses)}>
-                    <div className="place-img"><img src = {oneHouse.houses.xl_picture_url} alt='housepic' /></div>
-                    <h4>{oneHouse.houses.name}</h4>
-                    <h5>{oneHouse.houses.price}$ per night</h5>
-                    <div className="reviews">
-                    <StarRatingComponent 
-                    name="rate1" 
-                    editing={false}
-                    starCount={5}
-                    value={Math.round(oneHouse.houses.review_scores_rating/20)}
-                    />
-                        <h6>{oneHouse.houses.number_of_reviews}</h6>
-                    </div>
-                    </Link>
-                 </li>
-            )
-        })}
-        </ul>
+        { this.state.favorites.length !== 0 ?
+        <div className="col-lg-12 saved-list">
+          <ul>
+          {this.state.favorites.map(oneHouse=>{
+              return(
+                  <li key = {oneHouse.houses._id} className="col-lg-3 col-md-4 col-sm-6 col-xs-12">
+                      <Link to={houseUrl(oneHouse.houses)}>
+                      <div className="place-img"><img src = {oneHouse.houses.xl_picture_url} alt='housepic' /></div>
+                      <h4>{oneHouse.houses.name}</h4>
+                      <h5>{oneHouse.houses.price}$ per night</h5>
+                      <div className="reviews">
+                      <StarRatingComponent 
+                      name="rate1" 
+                      editing={false}
+                      starCount={5}
+                      value={Math.round(oneHouse.houses.review_scores_rating/20)}
+                      />
+                          <h6>{oneHouse.houses.number_of_reviews}</h6>
+                      </div>
+                      </Link>
+                  </li>
+              )
+          })}
+          </ul>
+        <Link to="/houses"><button className="booking-button h6">Find more places</button></Link>
+        </div>
+        :
+        <div className="col-lg-12 more">
+          <h1><b>0</b></h1>
+          <Link to="/houses"><button className="booking-button h6">Find more places</button></Link>
+        </div>
+      }
       </section>
     )
   }
