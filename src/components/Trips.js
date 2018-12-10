30020909ba590bc191098fcde56178cf22750751
Laura Stromboni <laura.stromboni@live.fr>
2018-12-10 11:37:52 +0100

    trips

--- src/components/Trips.js
+++ src/components/Trips.js
@@ -3,6 +3,10 @@ import {Link} from "react-router-dom";
 import axios from "axios"
 import "./style/Trips.scss"
 
+function houseUrl(oneBooking){
+  return `/houses/${oneBooking.houseId._id}`;
+}
+
 class Trips extends Component {
 
   constructor(props){
@@ -29,23 +33,32 @@ class Trips extends Component {
 
   render() {
     var moment = require('moment');
-    console.log(moment())
+    let todaysDate = new Date();
+    let afterToday = this.state.bookings.filter(oneBooking => {
+      return moment(oneBooking.arrayOfDates[0]).isAfter(todaysDate);
+    })
+    let beforeToday = this.state.bookings.filter(oneBooking => {
+      return moment(oneBooking.arrayOfDates[0]).isBefore(todaysDate);
+    })
+
     return(
       <section className="Trips">
         <div className="upcoming">
           <h2>Upcoming Trips</h2>
 
-          { this.state.bookings.length !== 0 ?
+          { afterToday.length !== 0 ?
           <div className="col-lg-12 saved-list">
             <ul>
-            {this.state.bookings.map((oneBooking, index)=>{
+            {afterToday.map((oneBooking, index)=>{
                 return(
                     <li key = {oneBooking.index} className="col-lg-3 col-md-4 col-sm-6 col-xs-12">
+                      <Link to={houseUrl(oneBooking)}>
                       <div className="place-img"><img src ={oneBooking.houseId.xl_picture_url} alt='housepic' /></div>
                       <h4>{oneBooking.houseId.name}</h4>
                       <p>{oneBooking.guests} guests</p>
                       <h5>{oneBooking.price}$ per night</h5>
                       <h5><b>From {oneBooking.arrayOfDates[0]} to {oneBooking.arrayOfDates[1]}</b></h5>
+                      </Link>
                     </li>
                 )
             })            
@@ -65,14 +78,31 @@ class Trips extends Component {
         <div className="been">
           <h2>Where you’ve been</h2>
 
-            {/* { now.isBefore(oneBooking.arrayOfDates[0]) ?            
-
-            : 
-
-            } */}
-
-          <p>You have not been anywhere yet. Check our cool places now !</p>
+            { beforeToday.length !== 0 ?
+              <div className="col-lg-12 saved-list">
+            <ul>
+            {beforeToday.map((oneBooking, index)=>{
+                return(
+                    <li key = {oneBooking.index} className="col-lg-3 col-md-4 col-sm-6 col-xs-12">
+                      <Link to={houseUrl(oneBooking)}>
+                      <div className="place-img"><img src ={oneBooking.houseId.xl_picture_url} alt='housepic' /></div>
+                      <h4>{oneBooking.houseId.name}</h4>
+                      <p>{oneBooking.guests} guests</p>
+                      <h5>{oneBooking.price}$ per night</h5>
+                      <h5><b>From {oneBooking.arrayOfDates[0]} to {oneBooking.arrayOfDates[1]}</b></h5>
+                      </Link>
+                    </li>
+                )
+            })            
+            }
+            </ul>
+          </div>
+          :
+          <div className="col-lg-12 more">
+            <p>You have not been anywhere yet. Check our cool places now !</p>
           <Link to="/houses"><button className="booking-button h6">Find places</button></Link>
+          </div>
+        }
         </div>
       </section>
     )
