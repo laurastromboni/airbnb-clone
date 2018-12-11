1ec9a254562405d83cfba9b37c0dc7f91366d489
Laura Stromboni <laura.stromboni@live.fr>
2018-12-11 15:59:16 +0100

    messages

--- src/api.js
+++ src/api.js
@@ -4,14 +4,14 @@ const socket = openSocket('http://localhost:5555/');
 
 function connect(cb) {
   socket.on('chat', (message) => {
-    console.log("chat!----------", message)
     NotificationManager.info('New message')
+    console.log("chat!----------", message)
     cb(message);
   })
 }
 
 function sendMessage(msg) {
-    socket.emit('chat message', msg);
+  socket.emit('chat message', msg);
 }
 
 export { connect, sendMessage }
